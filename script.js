5eab535dcc429ef8da1ecf17c92af104156cf93f
Rafael Santos Coelho <seu-email-do-github@gmail.com>
2025-04-11 22:23:55 -0300

    Adicionando imagens do carrossel

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
+const images = document.querySelectorAll('.carousel img');
 let index = 0;
-const slides = document.querySelectorAll('.carousel img');
 
-function showSlide(i) {
-  slides.forEach((img, idx) => {
-    img.classList.remove('active');
-    if (idx === i) {
-      img.classList.add('active');
-    }
-  });
-}
-
-function nextSlide() {
-  index = (index + 1) % slides.length;
-  showSlide(index);
-}
-
-setInterval(nextSlide, 3000); // Muda a cada 3 segundos
-
-document.addEventListener('DOMContentLoaded', () => {
-  showSlide(index);
-});
+setInterval(() => {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}, 3000);
